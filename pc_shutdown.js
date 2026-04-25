7fb25ba620e450aae846cdc6aa7760e7bff44c4e
vzopoiom-pixel <vzopoiom@gmail.com>
2026-04-25 17:26:56 +0200

    обновления кода и его обьяснения

--- pc_shutdown.js
+++ pc_shutdown.js
@@ -1,17 +1,18 @@
+// информация которая будет выводится на экране m5stick plus 2 
 var display = require("display");
 var wifi = require("wifi");
 var keyboard = require("keyboard");
 
 //вводим данные  о нашей wifi сети
-var PC_IP = "192.168.1.20";
-var PC_PORT = 8080;
-var WIFI_SSID = "Orange_Swiatlowod_C990";
-var WIFI_PASS = "HX5JDZLL5RNC";
+var PC_IP = "192.168.1.20"; // IP нашего пк 
+var PC_PORT = 8080; // порт который использует стик для подключения к пк
+var WIFI_SSID = "Orange_Swiatlowod_C990"; // названия нашей wifi сети 
+var WIFI_PASS = "HX5JDZLL5RNC"; // пароль от wifi 
 // до сюда
 
-var black = display.color(0, 0, 0);
+var black = display.color(0, 0, 0);  // цвет дисплей фона нашего стика
 var white = display.color(255, 255, 255);
-
+// очищает весь экран на нашем стике и делает его простым и понятным
 function drawUI(status) {
   display.fill(black);
   display.setTextSize(2);
@@ -21,7 +22,7 @@ function drawUI(status) {
   display.drawString("A=Shutdown B=Reboot", 10, 50);
   display.drawString(status, 10, 80);
 }
-
+// на дисплее пишется о том что мы подключаемся к wifi
 function sendCommand(cmd) {
   drawUI("Connecting WiFi...");
 
@@ -30,14 +31,14 @@ function sendCommand(cmd) {
   }
 
   if (!wifi.connected()) {
-    drawUI("WiFi Error!");
+    drawUI("WiFi Error!"); // пишет о том что у нас появились проблемы к подключению wifi не правильно веден пароль либо названия сети
     delay(2000);
     drawUI("Ready");
     return;
   }
 
   drawUI("Sending " + cmd + "...");
-
+//команда для выключения пк которакя водится через cmd 
   var res = wifi.httpFetch(
     "http://" + PC_IP + ":" + PC_PORT + "/" + cmd,
     { method: "GET" }
@@ -46,7 +47,7 @@ function sendCommand(cmd) {
   if (res && res.body) {
     drawUI("Done: " + cmd);
   } else {
-    drawUI("HTTP Error!");
+    drawUI("HTTP Error!"); // пишет о том что мы не смогли подключится к основной сети передакчи инфомации через инет HTTP
   }
 
   delay(2000);
@@ -54,7 +55,7 @@ function sendCommand(cmd) {
 }
 
 drawUI("Ready");
-
+// выключает пк
 while (true) {
   if (keyboard.getPrevPress()) {
     sendCommand("shutdown");
